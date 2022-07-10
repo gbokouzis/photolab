466eda9587467d496c869df5f5fbd0c7cd1295a1
gbokouzis <giwrgos.mp5555@gmail.com>
2022-07-11 00:25:05 +0300

    commit

--- app/Http/Requests/StoreImagePostRequest.php
+++ app/Http/Requests/StoreImagePostRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule as ValidationRule;
 
 class StoreImagePostRequest extends FormRequest
 {
@@ -26,7 +27,8 @@ class StoreImagePostRequest extends FormRequest
         return [
             'description' => 'required|min:3|max:50',
             'camera' => 'required|min:2|max:50',
-            'tags' => 'array'
+            'tags' => 'array',
+            'category_id' => ['required', ]
         ];
     }
 }
